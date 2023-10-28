34ab16d3bc5c8fd1722c3efffcc9b872eef29afb
Bahadir Turgut <bturgutny@gmail.com>
2023-10-28 11:54:51 -0400

    validation

--- src/main/java/com/bahd/controller/TransactionController.java
+++ src/main/java/com/bahd/controller/TransactionController.java
@@ -7,11 +7,13 @@ import com.bahd.service.AccountService;
 import com.bahd.service.TransactionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.Date;
 import java.util.UUID;
 
@@ -42,8 +44,13 @@ public class TransactionController {
     //write a post method that takes transaction object from the UI
     //complete the transfer and return the same page
     @PostMapping("/transfer")
-    public String makeTransfer(@ModelAttribute("transaction") Transaction transaction){
+    public String makeTransfer( @ModelAttribute("transaction") @Valid Transaction transaction, BindingResult bindingResult,Model model){
 
+        if(bindingResult.hasErrors()){
+            model.addAttribute("accounts", accountService.listAllAccount());
+            model.addAttribute("lastTransactions", transactionService.last10Transactions());
+            return "transaction/make-transfer";
+        }
         //I have UUID of accounts but I need to provide Account object
         // I need to find the Accounts based on the ID that I have and use a parameter to complete makeTransfer method
         Account sender =accountService.retrieveById(transaction.getSender());
